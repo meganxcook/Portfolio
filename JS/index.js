1c979aec907fb4d31dba671923604e74900ec250
meganxcook <cook.me.eliz@gmail.com>
2022-01-31 16:57:20 -0800

    updates

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,5 @@
+// Navigation
+
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
@@ -11,6 +13,8 @@ navLinks.forEach(link => {
     })
 })
 
+// Parallax
+
 const parallax = document.getElementById('header');
 
 window.addEventListener('scroll', function() {
@@ -18,8 +22,60 @@ window.addEventListener('scroll', function() {
     parallax.style.backgroundPositionY = offset * 0.7 + 'px';
 })
 
+// Scroll to top button
+
 const btnScrollToTop = document.getElementById('btn-scroll-to-top');
 
 btnScrollToTop.addEventListener('click', function() {
     window.scrollTo(0, 0);
 })
+
+// Experience bar graph
+
+// setTimeout(function start() {
+//     $('.bar').each(function(i) {
+//         let $bar = $(this);
+//         $(this).append('<span class="count"></span>')
+//         setTimeout(function() {
+//             $bar.css('width', $bar.attr('data-percent'));
+//         }, i * 100);
+//     });
+
+//     $('.count').each(function() {
+//         $(this).prop('Counter', 0).animate({
+//             Counter: $(this).parent('.bar').attr('data-percent')
+//         }, {
+//             duration: 2000,
+//             easing: 'swing',
+//             step: function(now) {
+//                 $(this).text(Math.ceil(now) + ' years');
+//             }
+//         });
+//     });
+
+// }, 500)
+
+
+
+setTimeout(function start() {
+    $('.bar').each(function() {
+        let bar = $(this);
+        $(this).append('<span class="count"></span>')
+        setTimeout(function() {
+            bar.css('width', bar.attr('data-percent'));
+        });
+    });
+
+    $('.count').each(function() {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).parent('.bar').attr('data-percent')
+        }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function(now) {
+                $(this).text(Math.ceil(now) + ' years');
+            }
+        });
+    });
+
+}, 1000)
