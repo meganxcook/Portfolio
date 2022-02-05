c39b7d535197bd19b252504c43a5ab701b9647da
meganxcook <cook.me.eliz@gmail.com>
2022-02-04 18:32:14 -0800

    updates

--- JS/index.js
+++ JS/index.js
@@ -13,6 +13,7 @@ navLinks.forEach(link => {
     })
 })
 
+
 // Parallax
 
 const parallax = document.getElementById('header');
@@ -22,6 +23,7 @@ window.addEventListener('scroll', function() {
     parallax.style.backgroundPositionY = offset * 0.7 + 'px';
 })
 
+
 // Scroll to top button
 
 const btnScrollToTop = document.getElementById('btn-scroll-to-top');
@@ -30,32 +32,13 @@ btnScrollToTop.addEventListener('click', function() {
     window.scrollTo(0, 0);
 })
 
+
+// Projects mouseover mouseout
+
+
+
+
 // Experience bar graph
-
-// setTimeout(function start() {
-//     $('.bar').each(function(i) {
-//         let $bar = $(this);
-//         $(this).append('<span class="count"></span>')
-//         setTimeout(function() {
-//             $bar.css('width', $bar.attr('data-percent'));
-//         }, i * 100);
-//     });
-
-//     $('.count').each(function() {
-//         $(this).prop('Counter', 0).animate({
-//             Counter: $(this).parent('.bar').attr('data-percent')
-//         }, {
-//             duration: 2000,
-//             easing: 'swing',
-//             step: function(now) {
-//                 $(this).text(Math.ceil(now) + ' years');
-//             }
-//         });
-//     });
-
-// }, 500)
-
-
 
 setTimeout(function start() {
     $('.bar').each(function() {
@@ -78,4 +61,4 @@ setTimeout(function start() {
         });
     });
 
-}, 1000)
+}, 750)
